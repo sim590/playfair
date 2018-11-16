e2f233bc396222fa3018694ae9f5567b97c59795
Simon Désaulniers <sim.desaulniers@gmail.com>
2018-11-16 07:44:33 -0500

    playfair: (dé)chiffrement du texte
    Le programme n'affiche que le chiffrement par défaut. Il faut activer le
    mode DEBUG (voir config.h) pour voir le déchiffrement.

--- playfair.c
+++ playfair.c
@@ -91,6 +91,56 @@ void convert_for_scheme_input(char** strp) {
     str = realloc(str, slen);
 }
 
+char* encrypt(const char* plaintext) {
+    size_t plen = strlen(plaintext);
+    char* ciphertext = (char*)calloc(plen+1, sizeof(char));
+    strcpy(ciphertext, plaintext);
+    for (size_t i = 0; i < plen; i+=2) {
+        i += strspn(plaintext+i, WHITESPACES);
+        size_t j = i+1;
+        j += strspn(plaintext+j, WHITESPACES);
+        size_t ki = strchr(key, plaintext[i])-key;
+        size_t kj = strchr(key, plaintext[j])-key;
+        if (ki/5 == kj/5) { /* sur la même ligne ! */
+            ciphertext[i] = key[(ki/5)*5+(ki+1)%5];
+            ciphertext[j] = key[(kj/5)*5+(kj+1)%5];
+        } else if (ki%5 == kj%5) { /* sur la même colonne ! */
+            ciphertext[i] = key[(ki+5)%25];
+            ciphertext[j] = key[(kj+5)%25];
+        } else { /* dans un rectangle ! */
+            ciphertext[i] = key[(ki/5)*5+kj%5];
+            ciphertext[j] = key[(kj/5)*5+ki%5];
+        }
+        i = j-1;
+    }
+    return ciphertext;
+}
+
+char* decrypt(const char* ciphertext) {
+    size_t clen = strlen(ciphertext);
+    char* plaintext = (char*)calloc(clen+1, sizeof(char));
+    strcpy(plaintext, ciphertext);
+    for (size_t i = 0; i < clen; i+=2) {
+        i += strspn(plaintext+i, WHITESPACES);
+        size_t j = i+1;
+        j += strspn(plaintext+j, WHITESPACES);
+        size_t ki = strchr(key, plaintext[i])-key;
+        size_t kj = strchr(key, plaintext[j])-key;
+        if (ki/5 == kj/5) { /* sur la même ligne ! */
+            plaintext[i] = key[(ki/5)*5+(ki-1)%5];
+            plaintext[j] = key[(kj/5)*5+(kj-1)%5];
+        } else if (ki%5 == kj%5) { /* sur la même colonne ! */
+            plaintext[i] = key[(ki-5)%25];
+            plaintext[j] = key[(kj-5)%25];
+        } else { /* dans un rectangle ! */
+            plaintext[i] = key[(ki/5)*5+kj%5];
+            plaintext[j] = key[(kj/5)*5+ki%5];
+        }
+        i = j-1;
+    }
+    return plaintext;
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 2)
         return 1;
@@ -126,6 +176,15 @@ int main(int argc, char *argv[]) {
     printf("%s\n\n", plaintext);
 #endif
 
+    char* ciphertext = encrypt(plaintext);
+
+    printf("%s\n", ciphertext);
+
+#if DEBUG==1
+    char* decryptedtext = decrypt(ciphertext);
+    printf("Texte déchiffré:\n");
+    printf("%s\n\n", decryptedtext);
+#endif
     return 0;
 }
 

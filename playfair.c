db561983afdc26d0637103e8b7b99925336da991
Simon Désaulniers <sim.desaulniers@gmail.com>
2018-11-16 06:28:21 -0500

    playfair: conversion lettres double (aa -> axax)
    * Les lettres en doubles sont converties en y ajoutant 'x'.
    * La dernière lettre à la fin d'un texte qui n'est pas en paire est
      aussi suffixé par 'x'.

--- playfair.c
+++ playfair.c
@@ -7,6 +7,13 @@
 #define BUFLEN 1024
 #define SELCHAR 'i'
 
+#define MIN_ASCII_CAPLETTER_VALUE 65
+#define MAX_ASCII_CAPLETTER_VALUE 90
+#define MIN_ASCII_LETTER_VALUE 97
+#define MAX_ASCII_LETTER_VALUE 122
+
+#define WHITESPACES " \f\n\r\t\v"
+
 char alphabet[25] = {
     'a', 'b', 'c',     'd', 'e',
     'f', 'g', 'h', SELCHAR, 'k',
@@ -17,6 +24,11 @@ char alphabet[25] = {
 
 char key[25];
 char BUF[BUFLEN];
+
+bool is_letter(char c) {
+    return (MIN_ASCII_LETTER_VALUE < c && c < MAX_ASCII_LETTER_VALUE)
+        || (MIN_ASCII_CAPLETTER_VALUE < c && c < MAX_ASCII_CAPLETTER_VALUE);
+}
 
 char* substr(char* str, size_t begin, size_t end) {
     size_t slen = end-begin+1;
@@ -54,6 +66,31 @@ void fill_key(const char* subkey) {
     fill_from_str(alphabet, subkey, cnt);
 }
 
+void convert_for_scheme_input(char** strp) {
+    char* str = *strp;
+    size_t slen = strlen(str);
+    size_t ssize = slen+1;
+    for (size_t i = 0; i < slen; i+=2) {
+        i += strspn(str+i, WHITESPACES);
+        size_t j = i+1;
+        j += strspn(str+j, WHITESPACES);
+        if (str[i] == str[j] || !is_letter(str[j])) {
+            if (strlen(str)+1 == ssize) {
+                ssize *= 2;
+                str = realloc(str, ssize);
+            }
+            char* rp = substr(str, i+1, strlen(str));
+            str[i+1] = 'x'; str[i+2] = '\0';
+            if (rp)
+                strcat(str, rp);
+            free(rp);
+            ++slen;
+        }
+        i = j-1;
+    }
+    str = realloc(str, slen);
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 2)
         return 1;
@@ -67,6 +104,7 @@ int main(int argc, char *argv[]) {
         if (i%5 == 4)
             printf("\n");
     }
+    printf("\n");
 #endif
 
     char* plaintext = malloc(sizeof(char));
@@ -77,7 +115,15 @@ int main(int argc, char *argv[]) {
     }
 
 #if DEBUG==1
-    printf("%s\n", plaintext);
+    printf("Texte clair:\n");
+    printf("%s\n\n", plaintext);
+#endif
+
+    convert_for_scheme_input(&plaintext);
+
+#if DEBUG==1
+    printf("Texte clair converti:\n");
+    printf("%s\n\n", plaintext);
 #endif
 
     return 0;
